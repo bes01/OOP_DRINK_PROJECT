c7dc05c0a1c1e278a39da875066f6e1154f0d767
bes01 <55254691+bes01@users.noreply.github.com>
2020-07-12 00:31:06 +0400

    test

--- Sasmelebi.Ge/src/main/java/Drinks/Constants/Constants.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Constants/Constants.java
@@ -1,7 +1,7 @@
 package Drinks.Constants;
 
 public class Constants {
-    public static String schema = "11111";
+    public static String schema = "1234";
     public static String dataBase = "";
     public static String dataUser = "";
     public static String dataPassword = "";
