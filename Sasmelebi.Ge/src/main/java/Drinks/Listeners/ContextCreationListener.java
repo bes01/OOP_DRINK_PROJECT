f1344969e1bd68fcd6e1dfa71d36153c61d7d460
bes01 <55254691+bes01@users.noreply.github.com>
2020-07-12 19:13:31 +0400

    ContextListener V2

--- Sasmelebi.Ge/src/main/java/Drinks/Listeners/ContextCreationListener.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Listeners/ContextCreationListener.java
@@ -4,7 +4,9 @@ import Drinks.Model.DataBase.Connector;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class ContextCreationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
