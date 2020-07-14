218a13078405ba6157028c06f54ddf6b8bc5ea62
bes01 <55254691+bes01@users.noreply.github.com>
2020-07-14 15:12:10 +0400

    Update SearchController.java

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/Search/SearchController.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Search/SearchController.java
@@ -1,11 +1,9 @@
 package Drinks.Controllers.Search;
 
-import Drinks.Constants.Constants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import sun.text.spi.JavaTimeDateTimePatternProvider;
 
 import javax.servlet.http.HttpServletRequest;
 
