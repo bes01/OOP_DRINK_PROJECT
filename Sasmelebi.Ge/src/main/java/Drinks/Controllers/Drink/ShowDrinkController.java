8fbbc0d10384c04c6aca0735f9e0e2bb6da21107
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 11:55:35 +0300

    სასმლის კონტროლერის ჩონჩხი

---
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/ShowDrinkController.java
@@ -0,0 +1,22 @@
+package Drinks.Controllers.Drink;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ShowDrinkController {
+
+    @GetMapping(value = "/Drink")
+    public ModelAndView getDrinkPage(HttpServletRequest request){
+        return null;
+    }
+
+    @PostMapping(value = "/Drink")
+    public ModelAndView postDrinkPage(HttpServletRequest request){
+        return null;
+    }
+}
