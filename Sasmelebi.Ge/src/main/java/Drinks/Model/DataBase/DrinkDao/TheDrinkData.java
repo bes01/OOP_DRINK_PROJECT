1577fb060cbd022ae3663777cbf504d25aa8e9fa
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 16:13:38 +0300

    ინგრედიენტების ამოღება

--- Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
@@ -11,8 +11,20 @@ public class TheDrinkData {
         return null;
     }
 
-    public ArrayList<Ingredient> getIngredients(int drinkId){
-        return null;
+    public ArrayList<Ingredient> getIngredients(int drinkId) throws SQLException {
+        Connector connector = Connector.getInstance();
+        PreparedStatement st = connector.getStatement(FROM_INGREDIENTS);
+        st.setString(1, Constants.schema + ".drinks_ingredients");
+        st.setString(2, Constants.schema + ".ingredients");
+        st.setString(3, Constants.schema + ".drinks_ingredients.ingredient_id");
+        st.setString(4, Constants.schema + ".ingredients.ingredient_id");
+        st.setInt(5, drinkId);
+        ResultSet res = connector.executeQuery(st);
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        while (res.next())
+            ingredients.add(new Ingredient(res.getInt("ingredient_id"),
+                    res.getString("ingredient_name")));
+        return ingredients;
     }
 
     public boolean isRated(int drinkId){
