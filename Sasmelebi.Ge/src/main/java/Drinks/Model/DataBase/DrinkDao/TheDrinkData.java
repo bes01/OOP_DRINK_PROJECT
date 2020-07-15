8e92f2496f354ad525d38964f234fbf84158ce93
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 16:12:04 +0300

    სასმელის ამოღება

--- Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
@@ -1,14 +1,27 @@
 package Drinks.Model.DataBase.DrinkDao;
 
+import Drinks.Constants.Constants;
 import Drinks.Model.Containers.Drink;
 import Drinks.Model.Containers.Ingredient;
+import Drinks.Model.DataBase.Connector;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class TheDrinkData {
 
-    public Drink getDrink(int drinkId) {
-        return null;
+    public Drink getDrink(int drinkId) throws SQLException {
+        Connector connector = Connector.getInstance();
+        PreparedStatement st = connector.getStatement(FROM_DRINKS);
+        st.setString(1, Constants.schema + ".drinks");
+        st.setInt(2, drinkId);
+        ResultSet res = connector.executeQuery(st);
+        ArrayList<Ingredient> ingredients = getIngredients(drinkId);
+        Drink drink = new Drink(res.getInt(1), res.getString(2), res.getString(3),
+                res.getString(4), res.getInt(5), res.getInt(6), ingredients);
+        return drink;
     }
 
     public ArrayList<Ingredient> getIngredients(int drinkId){
@@ -18,4 +31,7 @@ public class TheDrinkData {
     public boolean isRated(int drinkId){
         return false;
     }
+    public static final String FROM_DRINKS = "SELECT * FROM ? WHERE drink_id = ?";
+    public static final String FROM_INGREDIENTS = "SELECT * FROM ? " +
+            " JOIN ? ON ? = ? " + " WHERE drink_id = ?";
 }
