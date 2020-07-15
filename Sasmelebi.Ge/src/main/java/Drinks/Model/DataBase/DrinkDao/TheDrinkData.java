e05ab4a5548f4e0aa0293ac1a7b65907f3ec35c4
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 16:12:04 +0300

    სასმელის ამოღება

--- Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
@@ -1,52 +1,21 @@
 package Drinks.Model.DataBase.DrinkDao;
 
-import Drinks.Constants.Constants;
 import Drinks.Model.Containers.Drink;
 import Drinks.Model.Containers.Ingredient;
-import Drinks.Model.DataBase.Connector;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class TheDrinkData {
 
-    private Connector conn;
-
-    public TheDrinkData(){
-        conn = Connector.getInstance();
+    public Drink getDrink(int drinkId) {
+        return null;
     }
 
-    public Drink getDrink(int drinkId) throws SQLException {
-        Connector connector = Connector.getInstance();
-        PreparedStatement st = connector.getStatement(FROM_DRINKS);
-        st.setString(1, Constants.schema + ".drinks");
-        st.setInt(2, drinkId);
-        ResultSet res = connector.executeQuery(st);
-        ArrayList<Ingredient> ingredients = getIngredients(drinkId);
-        Drink drink = new Drink(res.getInt(1), res.getString(2), res.getString(3),
-                res.getString(4), res.getInt(5), res.getInt(6), ingredients);
-        return drink;
+    public ArrayList<Ingredient> getIngredients(int drinkId){
+        return null;
     }
 
-    public ArrayList<Ingredient> getIngredients(int drinkId) throws SQLException {
-        Connector connector = Connector.getInstance();
-        PreparedStatement st = connector.getStatement(FROM_INGREDIENTS);
-        st.setString(1, Constants.schema + ".drinks_ingredients");
-        st.setString(2, Constants.schema + ".ingredients");
-        st.setString(3, Constants.schema + ".drinks_ingredients.ingredient_id");
-        st.setString(4, Constants.schema + ".ingredients.ingredient_id");
-        st.setInt(5, drinkId);
-        ResultSet res = connector.executeQuery(st);
-        ArrayList<Ingredient> ingredients = new ArrayList<>();
-        while (res.next())
-            ingredients.add(new Ingredient(res.getInt("ingredient_id"),
-                    res.getString("ingredient_name")));
-        return ingredients;
+    public boolean isRated(int drinkId){
+        return false;
     }
-    public static final String FROM_DRINKS = "SELECT * FROM ? WHERE drink_id = ?";
-    public static final String FROM_INGREDIENTS = "SELECT * FROM ? " +
-            " JOIN ? ON ? = ? " + " WHERE drink_id = ?";
 }
