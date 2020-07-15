7cb4e82986786ad6b3ade8f1c3036f9538a358d8
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 16:53:42 +0300

    გეთის განახლება

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/DrinkController.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/DrinkController.java
@@ -1,18 +1,26 @@
 package Drinks.Controllers.Drink;
 
+import Drinks.Model.Containers.Drink;
+import Drinks.Model.DataBase.DrinkDao.TheDrinkData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 
 @Controller
 public class DrinkController {
 
     @GetMapping(value = "/Drink")
-    public ModelAndView getDrinkPage(HttpServletRequest request){
-        return null;
+    public ModelAndView getDrinkPage(HttpServletRequest request) throws SQLException {
+        String drinkId = request.getParameter("drink_id");
+        ModelAndView mw = new ModelAndView("/Drink/showDrink");
+        TheDrinkData dt = new TheDrinkData();
+        Drink dr = dt.getDrink(Integer.valueOf(drinkId));
+        mw.addObject("drink");
+        return mw;
     }
 
     @PostMapping(value = "/Drink")
