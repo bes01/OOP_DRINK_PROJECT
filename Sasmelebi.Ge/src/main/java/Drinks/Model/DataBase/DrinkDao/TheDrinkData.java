dd12c90c115f6133556233179d2f8bda9d5b4183
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 16:10:00 +0300

    სასმელისთვის ინფოს ამოღება ბაზიდან

---
+++ Sasmelebi.Ge/src/main/java/Drinks/Model/DataBase/DrinkDao/TheDrinkData.java
@@ -0,0 +1,21 @@
+package Drinks.Model.DataBase.DrinkDao;
+
+import Drinks.Model.Containers.Drink;
+import Drinks.Model.Containers.Ingredient;
+
+import java.util.ArrayList;
+
+public class TheDrinkData {
+
+    public Drink getDrink(int drinkId) {
+        return null;
+    }
+
+    public ArrayList<Ingredient> getIngredients(int drinkId){
+        return null;
+    }
+
+    public boolean isRated(int drinkId){
+        return false;
+    }
+}
