94e8c9a6fa8be5a8c086b2d18e44a6eac536b436
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-15 13:25:22 +0300

    მცირედი განახლება გეთში

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/ShowDrinkController.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/ShowDrinkController.java
@@ -1,5 +1,7 @@
 package Drinks.Controllers.Drink;
 
+import Drinks.Model.Containers.Drink;
+import Drinks.Model.DataBase.DrinkDao.TheDrinkData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,12 @@ public class ShowDrinkController {
 
     @GetMapping(value = "/Drink")
     public ModelAndView getDrinkPage(HttpServletRequest request){
-        return null;
+        String drinkId = request.getParameter("drinkId");
+        ModelAndView mw = new ModelAndView("/Drink");
+        TheDrinkData dt = new TheDrinkData();
+        Drink dr = dt.getDrink(drinkId);
+        mw.addObject("drink");
+        return mw;
     }
 
     @PostMapping(value = "/Drink")
