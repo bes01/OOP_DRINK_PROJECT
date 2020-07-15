c0a6db9ea94a5b422f70ed21ca9226234a1219f2
LortkipaHub <56543368+LortkipaHub@users.noreply.github.com>
2020-07-16 01:01:01 +0400

    commit stub

--- Sasmelebi.Ge/src/main/java/Drinks/Constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package Drinks.Constants;
-
-public class Constants {
-    public static String schema = "1234";
-    public static String dataBase = "";
-    public static String dataUser = "";
-    public static String dataPassword = "";
-}
-
