1b49db836ce64bf0a01e1cdeb83d6ab03c6e4892
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-16 22:18:00 +0300

    ვრაპერი კლასის მოდიფიკაცია

--- Sasmelebi.Ge/src/main/java/Drinks/Model/Containers/DrinkFull.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Model/Containers/DrinkFull.java
@@ -1,17 +1,22 @@
 package Drinks.Model.Containers;
 
+import Drinks.Model.DataBase.DataRetrieve.UserData;
 import Drinks.Model.DataBase.DrinkDao.TheDrinkData;
 
+import java.sql.Array;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DrinkFull {
     
-    int user_ranking, all_ranking;
+    private int user_ranking, all_ranking;
+    private Drink parent;
     
     public DrinkFull(int drink_id, int user_id) throws SQLException {
         TheDrinkData drData = new TheDrinkData();
         all_ranking = drData.sumRated(drink_id);
         user_ranking = drData.userRated(drink_id, user_id);
+        parent = drData.getParentDrink(drink_id);
     }
     
     public int getCurrentRanking() {
@@ -21,4 +26,6 @@ public class DrinkFull {
     public int getCurrentUserRanking() {
         return user_ranking;
     }
+
+    public Drink getParentDrink(){ return parent; }
 }
