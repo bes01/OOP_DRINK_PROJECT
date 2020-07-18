b0d191b942af0e50fdfa13a374c353cc1d5fa431
Miralllll <55250021+Miralllll@users.noreply.github.com>
2020-07-18 23:26:45 +0300

    აქაც იუზერ აიდიო

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/UserController.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Drink/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     @GetMapping(value = "/User")
     public ModelAndView getUserPage(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException {
-        User user = (User) request.getSession().getAttribute("user");
+        User user = (User) request.getSession().getAttribute("user_id");
         if(user == null) {
             response.sendRedirect("/");
             return null;
@@ -37,7 +37,7 @@ public class UserController {
 
     @GetMapping(value = "/Drink/UserNotFound")
     public ModelAndView getUserNotFoundPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        User user = (User) request.getSession().getAttribute("user");
+        User user = (User) request.getSession().getAttribute("user_id");
         if(user == null) {
             response.sendRedirect("/");
             return null;
