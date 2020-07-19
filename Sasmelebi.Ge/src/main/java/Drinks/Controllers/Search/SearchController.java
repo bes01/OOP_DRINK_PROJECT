87a087db8f61268feea913fdaa088fdf53584868
LortkipaHub <56543368+LortkipaHub@users.noreply.github.com>
2020-07-19 23:50:03 +0400

    added combobox empty values filters

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/Search/SearchController.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/Search/SearchController.java
@@ -16,13 +16,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-
 @Controller
 public class SearchController {
 
     @RequestMapping(value = "/Search", method = RequestMethod.GET)
-    public ModelAndView getSearchPage(HttpServletRequest request , HttpServletResponse response) throws IOException {
-        if (request.getSession().getAttribute("user_id")==null){
+    public ModelAndView getSearchPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getSession().getAttribute("user_id") == null) {
             response.sendRedirect("/");
             return null;
         }
@@ -34,9 +33,9 @@ public class SearchController {
         int[] ingredientIds = getIngredientIds(ingredientIdsTemp);
         IngredientData ingredientDB = (IngredientData) request.getServletContext().getAttribute("ingredientData");
         ArrayList<Ingredient> ingredients = ingredientDB.getAllIngredients();
-        if(drinkName != null) {
+        if (drinkName != null) {
             DrinkData drinkDb = (DrinkData) request.getServletContext().getAttribute("drinkData");
-            ArrayList<Drink> drinks = drinkDb.getDrinksByNameAndIngredients(drinkName,ingredientIds);
+            ArrayList<Drink> drinks = drinkDb.getDrinksByNameAndIngredients(drinkName, ingredientIds);
             ret.addObject("drinks", drinks);
         }
 
@@ -48,14 +47,31 @@ public class SearchController {
     }
 
     private int[] getIngredientIds(String[] parameters) {
-        int[] ingredientIds = null;
-        if (parameters != null) {
-            ingredientIds = new int[parameters.length];
-            for (int i = 0; i < parameters.length; i++) {
-                ingredientIds[i] = Integer.parseInt(parameters[i]);
-            }
+        ArrayList<String> filteredParameters = getFilteredParameters(parameters);
+        if (filteredParameters.size() == 0)
+            return null;
+        int[] ingredientIds = new int[filteredParameters.size()];
+        for (int i = 0; i < filteredParameters.size(); i++) {
+            ingredientIds[i] = Integer.parseInt(filteredParameters.get(i));
+
         }
         return ingredientIds;
+    }
+
+
+
+
+    private ArrayList<String> getFilteredParameters(String[] parameters) {
+        ArrayList<String> filteredParameters = new ArrayList<>();
+        if (parameters == null)
+            return filteredParameters;
+        for (int i = 0; i < parameters.length; i++) {
+            if (!parameters[i].equals("empty"))
+                filteredParameters.add(parameters[i]);
+
+        }
+        return filteredParameters;
+
 
     }
 }
