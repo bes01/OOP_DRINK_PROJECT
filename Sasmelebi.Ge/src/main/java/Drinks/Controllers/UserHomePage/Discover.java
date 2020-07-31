d9eacfdf72ddd899e063bde8a09b79dc97db38e6
bes01 <55254691+bes01@users.noreply.github.com>
2020-07-31 16:41:39 +0400

    Driscover

--- Sasmelebi.Ge/src/main/java/Drinks/Controllers/UserHomePage/Discover.java
+++ Sasmelebi.Ge/src/main/java/Drinks/Controllers/UserHomePage/Discover.java
@@ -14,17 +14,17 @@ import java.util.ArrayList;
 
 @Controller
 public class Discover {
-//    @GetMapping("/Discover")
-//    public ModelAndView renderDiscoverPage(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
-//        if (request.getSession().getAttribute("user") == null) {
-//            response.sendRedirect("/logout");
-//            return null;
-//        }
-//        ModelAndView maw = new ModelAndView("/UserHomePage/discover");
-//        DiscoverData discoverData = new DiscoverData();
-//        maw.addObject("recentlyAdded", discoverData.recentlyAdded());
-//        maw.addObject("topDrinks", discoverData.topDrinks());
-//        maw.addObject("randomDrinks", discoverData.randomDrinks());
-//        return maw;
-//    }
+    @GetMapping("/Discover")
+    public ModelAndView renderDiscoverPage(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+        if (request.getSession().getAttribute("user_id") == null) {
+            response.sendRedirect("/logout");
+            return null;
+        }
+        ModelAndView maw = new ModelAndView("/UserHomePage/discover");
+        DiscoverData discoverData = new DiscoverData();
+        maw.addObject("recentlyAdded", discoverData.recentlyAdded());
+        maw.addObject("topDrinks", discoverData.topDrinks());
+        maw.addObject("randomDrinks", discoverData.randomDrinks());
+        return maw;
+    }
 }
